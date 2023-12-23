6c8346ae528da1cefd8961bd31c26c3da34ad40d
AR <aarokan@gmail.com>
2023-12-23 20:05:26 +0300

    create an array of binary

--- bulbs.c
+++ bulbs.c
@@ -49,7 +49,79 @@ void print_bulb(int bit)
 
 int convertToBinary(decimal)
 {
-    printf("decimal test = %i", decimal);
+    int binaryArray[8];
+    if (decimal > 128)
+    {
+        binaryArray[7] = 1;
+    }
+    else if (decimal < 128)
+    {
+        binaryArray[7] = 0;
+    }
+    else if (decimal > 64)
+    {
+        binaryArray[6] = 1;
+    }
+    else if (decimal < 64)
+    {
+        binaryArray[6] = 0;
+    }
+    else if (decimal > 32)
+    {
+        binaryArray[5] = 1;
+    }
+    else if (decimal < 32)
+    {
+        binaryArray[5] = 0;
+    }
+    else if (decimal > 16)
+    {
+        binaryArray[4] = 1;
+    }
+    else if (decimal < 16)
+    {
+        binaryArray[4] = 0;
+    }
+    else if (decimal > 8)
+    {
+        binaryArray[3] = 1;
+    }
+    else if (decimal < 8)
+    {
+        binaryArray[3] = 0;
+    }
+    else if (decimal > 4)
+    {
+        binaryArray[2] = 1;
+    }
+    else if (decimal < 4)
+    {
+        binaryArray[2] = 0;
+    }
+    else if (decimal > 2)
+    {
+        binaryArray[1] = 1;
+    }
+    else if (decimal < 2)
+    {
+        binaryArray[1] = 0;
+    }
+    else if (decimal > 1)
+    {
+        binaryArray[0] = 1;
+    }
+    else
+    {
+        binaryArray[0] = 0;
+    }
+    printf("\nBinaray Array =  %i", binaryArray[7]);
+    printf("%i", binaryArray[6]);
+    printf("%i", binaryArray[5]);
+    printf("%i", binaryArray[4]);
+    printf("%i", binaryArray[3]);
+    printf("%i", binaryArray[2]);
+    printf("%i", binaryArray[1]);
+    printf("%i", binaryArray[0]);
 
     return 0;
 }
