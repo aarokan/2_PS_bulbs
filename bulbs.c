9e4db421db8c0865e65640499e4d32d7db957739
AR <aarokan@gmail.com>
2023-12-25 23:52:16 +0300

    seperate conditionals

--- bulbs.c
+++ bulbs.c
@@ -53,75 +53,107 @@ int convertToBinary(decimal)
     if (decimal > 128)
     {
         binaryArray[7] = 1;
+        printf("\nDecimal 128 = %i", decimal);
+        decimal -= 128;
     }
-    else if (decimal < 128)
+    else if (decimal <= 128)
     {
         binaryArray[7] = 0;
+        printf("\nDecimal 128 = %i", decimal);
     }
-    else if (decimal > 64)
+
+    if (decimal > 64)
     {
         binaryArray[6] = 1;
+        decimal -= 64;
+        printf("\nDecimal 64 = %i", decimal);
     }
-    else if (decimal < 64)
+    else if (decimal <= 64)
     {
         binaryArray[6] = 0;
+        printf("\nDecimal 64 = %i", decimal);
     }
-    else if (decimal > 32)
+    
+    if (decimal > 32)
     {
         binaryArray[5] = 1;
+        printf("\nDecimal 32 = %i", decimal);
+        decimal -= 32;
     }
-    else if (decimal < 32)
+    else if (decimal <= 32)
     {
         binaryArray[5] = 0;
+        printf("\nDecimal 32 = %i", decimal);
     }
-    else if (decimal > 16)
+    
+    if (decimal > 16)
     {
         binaryArray[4] = 1;
+        printf("\nDecimal 16 = %i", decimal);
+        decimal -= 16;
     }
-    else if (decimal < 16)
+    else if (decimal <= 16)
     {
         binaryArray[4] = 0;
+        printf("\nDecimal 16 = %i", decimal);
     }
-    else if (decimal > 8)
+
+    if (decimal > 8)
     {
         binaryArray[3] = 1;
+        printf("\nDecimal 8 = %i", decimal);
+        decimal -= 8;
     }
-    else if (decimal < 8)
+    else if (decimal <= 8)
     {
         binaryArray[3] = 0;
+        printf("\nDecimal 8 = %i", decimal);
     }
-    else if (decimal > 4)
+    
+    if (decimal > 4)
     {
         binaryArray[2] = 1;
+        printf("\nDecimal 4 = %i", decimal);
+        decimal -= 4;
     }
-    else if (decimal < 4)
+    else if (decimal <= 4)
     {
         binaryArray[2] = 0;
+        printf("\nDecimal 4 = %i", decimal);
     }
-    else if (decimal > 2)
+    
+    if (decimal > 2)
     {
         binaryArray[1] = 1;
+        printf("\nDecimal 2 = %i", decimal);
+        decimal -= 2;
     }
-    else if (decimal < 2)
+    else if (decimal <= 2)
     {
         binaryArray[1] = 0;
+        printf("\nDecimal 2 = %i", decimal);
     }
-    else if (decimal > 1)
+    
+    if (decimal > 1)
     {
         binaryArray[0] = 1;
+        printf("\nDecimal 1 = %i", decimal);
+        decimal -= 1;
     }
     else
     {
         binaryArray[0] = 0;
+        printf("\nDecimal 1 = %i", decimal);
     }
+    
     printf("\nBinaray Array =  %i", binaryArray[7]);
-    printf("%i", binaryArray[6]);
-    printf("%i", binaryArray[5]);
-    printf("%i", binaryArray[4]);
-    printf("%i", binaryArray[3]);
-    printf("%i", binaryArray[2]);
-    printf("%i", binaryArray[1]);
-    printf("%i", binaryArray[0]);
+    printf("\n%i", binaryArray[6]);
+    printf("\n%i", binaryArray[5]);
+    printf("\n%i", binaryArray[4]);
+    printf("\n%i", binaryArray[3]);
+    printf("\n%i", binaryArray[2]);
+    printf("\n%i", binaryArray[1]);
+    printf("\n%i", binaryArray[0]);
 
     return 0;
 }
