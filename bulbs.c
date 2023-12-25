037ccf3641c8d8e46687626f0b71013745e2f076
AR <aarokan@gmail.com>
2023-12-26 00:18:33 +0300

    print bulb

--- bulbs.c
+++ bulbs.c
@@ -23,15 +23,17 @@ int main(void)
     for (int i = 0; i < msgLength; i++ )
     {
         // convert character to a decimal
-        printf("\ncurrent character = %c", message[i]);
         int decimalEquivalent = message[i];
-        printf("\nconverted to decimal = %i", decimalEquivalent);
-        printf("\n");
 
         // convert decimal to binary
         convertToBinary(decimalEquivalent);
 
         // print binary using print_bulb
+        for (int bit = 0; bit < 8; bit ++)
+        {
+            print_bulb(binaryArray[bit]);
+        }
+        printf("\n");
     }
 }
 
@@ -53,81 +55,81 @@ void convertToBinary(decimal)
 {
     if (decimal >= 128)
     {
-        binaryArray[7] = 1;
+        binaryArray[0] = 1;
         decimal -= 128;
     }
     else
     {
-        binaryArray[7] = 0;
+        binaryArray[0] = 0;
     }
 
     if (decimal >= 64)
     {
-        binaryArray[6] = 1;
-        decimal -= 64;
-    }
-    else
-    {
-        binaryArray[6] = 0;
-    }
-    
-    if (decimal >= 32)
-    {
-        binaryArray[5] = 1;
-        decimal -= 32;
-    }
-    else
-    {
-        binaryArray[5] = 0;
-    }
-    
-    if (decimal >= 16)
-    {
-        binaryArray[4] = 1;
-        decimal -= 16;
-    }
-    else
-    {
-        binaryArray[4] = 0;
-    }
-
-    if (decimal >= 8)
-    {
-        binaryArray[3] = 1;
-        decimal -= 8;
-    }
-    else
-    {
-        binaryArray[3] = 0;
-    }
-    
-    if (decimal >= 4)
-    {
-        binaryArray[2] = 1;
-        decimal -= 4;
-    }
-    else
-    {
-        binaryArray[2] = 0;
-    }
-    
-    if (decimal >= 2)
-    {
         binaryArray[1] = 1;
-        decimal -= 2;
+        decimal -= 64;
     }
     else
     {
         binaryArray[1] = 0;
     }
     
+    if (decimal >= 32)
+    {
+        binaryArray[2] = 1;
+        decimal -= 32;
+    }
+    else
+    {
+        binaryArray[2] = 0;
+    }
+    
+    if (decimal >= 16)
+    {
+        binaryArray[3] = 1;
+        decimal -= 16;
+    }
+    else
+    {
+        binaryArray[3] = 0;
+    }
+
+    if (decimal >= 8)
+    {
+        binaryArray[4] = 1;
+        decimal -= 8;
+    }
+    else
+    {
+        binaryArray[4] = 0;
+    }
+    
+    if (decimal >= 4)
+    {
+        binaryArray[5] = 1;
+        decimal -= 4;
+    }
+    else
+    {
+        binaryArray[5] = 0;
+    }
+    
+    if (decimal >= 2)
+    {
+        binaryArray[6] = 1;
+        decimal -= 2;
+    }
+    else
+    {
+        binaryArray[6] = 0;
+    }
+    
     if (decimal >= 1)
     {
-        binaryArray[0] = 1;
+        binaryArray[7] = 1;
         decimal -= 1;
     }
     else
     {
-        binaryArray[0] = 0;
+        binaryArray[7] = 0;
     }
 }
