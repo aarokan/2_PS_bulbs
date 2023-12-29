32980745f72ea79b26ea7963bc5da2a10c7f512e
AR <aarokan@gmail.com>
2023-12-29 22:53:44 +0300

    start new way to solve problem

--- bulbs.c
+++ bulbs.c
@@ -20,7 +20,7 @@ int main(void)
     int msgLength = strlen(message);
 
     // loop through message
-    for (int i = 0; i < msgLength; i++ )
+    for (int i = 0; i < msgLength; i++)
     {
         // convert character to a decimal
         int decimalEquivalent = message[i];
@@ -29,7 +29,7 @@ int main(void)
         convertToBinary(decimalEquivalent);
 
         // print binary using print_bulb
-        for (int bit = 0; bit < 8; bit ++)
+        for (int bit = 0; bit < 8; bit++)
         {
             print_bulb(binaryArray[bit]);
         }
@@ -53,83 +53,5 @@ void print_bulb(int bit)
 
 void convertToBinary(decimal)
 {
-    if (decimal >= 128)
-    {
-        binaryArray[0] = 1;
-        decimal -= 128;
-    }
-    else
-    {
-        binaryArray[0] = 0;
-    }
-
-    if (decimal >= 64)
-    {
-        binaryArray[1] = 1;
-        decimal -= 64;
-    }
-    else
-    {
-        binaryArray[1] = 0;
-    }
-    
-    if (decimal >= 32)
-    {
-        binaryArray[2] = 1;
-        decimal -= 32;
-    }
-    else
-    {
-        binaryArray[2] = 0;
-    }
-    
-    if (decimal >= 16)
-    {
-        binaryArray[3] = 1;
-        decimal -= 16;
-    }
-    else
-    {
-        binaryArray[3] = 0;
-    }
-
-    if (decimal >= 8)
-    {
-        binaryArray[4] = 1;
-        decimal -= 8;
-    }
-    else
-    {
-        binaryArray[4] = 0;
-    }
-    
-    if (decimal >= 4)
-    {
-        binaryArray[5] = 1;
-        decimal -= 4;
-    }
-    else
-    {
-        binaryArray[5] = 0;
-    }
-    
-    if (decimal >= 2)
-    {
-        binaryArray[6] = 1;
-        decimal -= 2;
-    }
-    else
-    {
-        binaryArray[6] = 0;
-    }
-    
-    if (decimal >= 1)
-    {
-        binaryArray[7] = 1;
-        decimal -= 1;
-    }
-    else
-    {
-        binaryArray[7] = 0;
-    }
+    printf("\nDecimal = %i", decimal);
 }
